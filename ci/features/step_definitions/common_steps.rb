0e203f94e6b40f1ca431adaff301393a35318f8d
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-12-06 15:50:14 +0000

    split out step definitions

--- ci/features/step_definitions/common_steps.rb
+++ ci/features/step_definitions/common_steps.rb
@@ -79,30 +79,3 @@ LITERAL_SECRET: a literal secret
 EOS
   File.open("#{@BUILD_DIR}/secrets.yml", 'w') { |file| file.write(secretsyml) }
 end
-
-Then(/^the environment contains the secret values as per secrets\.yml$/) do
-  step "the 'env' command is run"
-  expect(@output).to include("CONJUR_SECRET=a conjur secret")
-  expect(@output).to include("LITERAL_SECRET=a literal secret")
-end
-
-When(/^the \.profile\.d script is sourced$/) do
-  @commands ||= []
-  @commands << <<EOL
-. #{@BUILD_DIR}/.profile.d/0000_retrieve-secrets.sh #{@BUILD_DIR}
-EOL
-end
-
-Then(/^summon is installed$/) do
-  `#{@BUILD_DIR}/summon -v`
-  expect($?.exitstatus).to eq (0)
-end
-
-Then(/^summon-conjur is installed$/) do
-  `#{@BUILD_DIR}/summon-conjur -v`
-  expect($?.exitstatus).to eq (0)
-end
-
-Then(/^the retrieve secrets \.profile\.d script is installed$/) do
-  expect(File.exist?("#{@BUILD_DIR}/.profile.d/0000_retrieve-secrets.sh")).to be_truthy
-end
