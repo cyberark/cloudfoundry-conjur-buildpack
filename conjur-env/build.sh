d105f9ee50099d46d8300283c7e9075c10635ab5
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-02-04 20:23:52 +0000

    prune dangling docker

--- conjur-env/build.sh
+++ conjur-env/build.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 # http://blog.wrouesnel.com/articles/Totally%20static%20Go%20builds/
-docker run -e CGO_ENABLED=0 -e GOOS=linux -v "$PWD":/go/src/conjur-env -w /go/src/conjur-env golang:1.8 go build -o conjur-env -a -ldflags '-extldflags "-static"' .
+CGO_ENABLED=0 GOOS=linux go build -o conjur-env -a -ldflags '-extldflags "-static"' .
 
 mkdir -p ../deps
 mv conjur-env ../deps
