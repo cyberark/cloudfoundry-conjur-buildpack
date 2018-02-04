1e0fa92957e42174d4d061faa30f9f3c89bccd2a
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-02-04 21:50:54 +0000

    no tty

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 . ./docker_vars.sh
 
-docker run --rm -it -v "$PWD":/buildpack -w /buildpack golang:1.8-alpine3.5 ./conjur-env/build.sh
+docker run --rm -v "$PWD":/buildpack -w /buildpack golang:1.8-alpine3.5 ./conjur-env/build.sh
 
 docker-compose build
 docker-compose run --rm tester ./package_buildpack.sh
